42a2c1c83a4895f104f26547c1e9cc70789095d3
Jeffreywangjun <wangjunahasd@163.com>
2018-07-22 23:13:02 +0800

    可以解析简单的json文件，和嵌套的json文件
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- MyJson/app/src/main/java/com/jeffrey/myjson/MainActivity.java
+++ MyJson/app/src/main/java/com/jeffrey/myjson/MainActivity.java
@@ -15,13 +15,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-    public static String DATA_URL  = "http://10.0.2.2:8080/WEB12/live.json";
+    // 本地服务器地址
+    //public static String DATA_URL = "http://10.0.2.2:8080/WEB12/live.json";
+    //public static String DATA_URL = "http://10.0.2.2:8080/WEB12/home_data.json";
+  //  public static String DATA_URL = "http://10.0.2.2:8080/WEB12/test.json";
+
+    public static String DATA_URL = "http://10.0.2.2:8080/WEB12/home_data_test.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 getJsonArray(DATA_URL);
@@ -29,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         }.start();
     }
 
+    /**
+     * 根据本地url地址得到json数据字符串
+     * @param url
+     * @return
+     */
     public JSONArray getJsonArray(String url) {
         JSONArray jsonArray = null;
         HttpClient client = new DefaultHttpClient();
@@ -39,19 +49,22 @@ public class MainActivity extends AppCompatActivity {
             HttpResponse response = client.execute(get);
             BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
             String s = reader.readLine();
-            for (;s != null; s = reader.readLine()) {
+            for (; s != null; s = reader.readLine()) {
                 builder.append(s);
             }
+          //  Log.e("XXX", "wangjun------builder--:" + builder.toString());
             JSONObject jo = new JSONObject(builder.toString());
-            JSONArray jor = jo.getJSONArray("result");
+           /* JSONArray jor = jo.getJSONArray("result");
             JSONObject jol = (JSONObject) jor.get(0);
-            jsonArray = jol.getJSONArray("list");
+            jsonArray = jol.getJSONArray("list");*/
+
+           JSONObject dataObject = jo.getJSONObject("data");
+           JSONObject headObject = dataObject.getJSONObject("head");
+            jsonArray = headObject.getJSONArray("ads");
             Log.e("XXX", "wangjun------jsonArray--:" + jsonArray);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
         return jsonArray;
     }
 

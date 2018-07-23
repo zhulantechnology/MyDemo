e7a7f9d470ccbb9c661ecbc791d1d72a64ce0911
Jeffreywangjun <wangjunahasd@163.com>
2018-07-23 11:39:59 +0800

    几种获取不同目录的json文件内容，转成字符串
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- MyJson/app/src/main/java/com/jeffrey/myjson/MainActivity.java
+++ MyJson/app/src/main/java/com/jeffrey/myjson/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jeffrey.myjson;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -12,15 +15,18 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     // 本地服务器地址
-    //public static String DATA_URL = "http://10.0.2.2:8080/WEB12/live.json";
-    //public static String DATA_URL = "http://10.0.2.2:8080/WEB12/home_data.json";
-  //  public static String DATA_URL = "http://10.0.2.2:8080/WEB12/test.json";
+    //public static String DATA_URL = "http://10.0.2.2:8080/MyWEB13/live.json";
+    //public static String DATA_URL = "http://10.0.2.2:8080/MyWEB13/home_data.json";
+    //  public static String DATA_URL = "http://10.0.2.2:8080/MyWEB13/test.json";
 
-    public static String DATA_URL = "http://10.0.2.2:8080/WEB12/home_data_test.json";
+    public static String DATA_URL = "http://10.0.2.2:8080/MyWEB13/home_data_test.json";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +35,49 @@ public class MainActivity extends AppCompatActivity {
         new Thread() {
             @Override
             public void run() {
-                getJsonArray(DATA_URL);
+                // getJsonArray(DATA_URL);
+                // getJsonFromIO(DATA_URL);
+                String str = getAssetsJson(MainActivity.this, "home_data_test.json");
+                Log.e("XXX", "WANGJUN----------str---:" + str);
             }
         }.start();
     }
 
+    // 获取本地assets文件夹下的json文件
+    public static String getAssetsJson(Context context, String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        //获得assets资源管理器
+        AssetManager assetManager = context.getAssets();
+        //使用IO流读取json文件内容
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    assetManager.open(fileName), "utf-8"));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+    //  使用 compile 'commons-io:commons-io:2.4' 获取文件内容
+    private void getJsonFromIO(String url) {
+        File file = new File("src/main/java/home_data_test.json");
+        try {
+            String content = FileUtils.readFileToString(file);
+            Log.e("XXX", "WANGJUN----------content---:" + content);
+        } catch (Exception e) {
+            Log.e("XXX", "WANGJUN----------e---:" + e);
+            e.printStackTrace();
+        }
+
+    }
+
     /**
      * 根据本地url地址得到json数据字符串
+     *
      * @param url
      * @return
      */
@@ -52,14 +94,16 @@ public class MainActivity extends AppCompatActivity {
             for (; s != null; s = reader.readLine()) {
                 builder.append(s);
             }
-          //  Log.e("XXX", "wangjun------builder--:" + builder.toString());
-            JSONObject jo = new JSONObject(builder.toString());
+
+
+            Log.e("XXX", "wangjun------builder--:" + builder.toString());
+            JSONObject jo = new JSONObject(builder.toString());  // 这个builer.toString需要满足json数据的key-value格式
            /* JSONArray jor = jo.getJSONArray("result");
             JSONObject jol = (JSONObject) jor.get(0);
             jsonArray = jol.getJSONArray("list");*/
 
-           JSONObject dataObject = jo.getJSONObject("data");
-           JSONObject headObject = dataObject.getJSONObject("head");
+            JSONObject dataObject = jo.getJSONObject("data");
+            JSONObject headObject = dataObject.getJSONObject("head");
             jsonArray = headObject.getJSONArray("ads");
             Log.e("XXX", "wangjun------jsonArray--:" + jsonArray);
         } catch (Exception e) {
